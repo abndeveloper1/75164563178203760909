06ff94542f3306cfb547376df72a603a49fba058
thanhson <steven.n.t@outlook.com>
2022-11-13 19:01:27 +0700

    pixabaytg

---
+++ pixabaytg/script-parse-telegram-export-1.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+fs.readFile('result.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  //console.log(data);
+
+ var mydata = JSON.parse(data).messages;
+ for (let i = 0; i < mydata.length; i++) {
+  console.log(mydata[i].photo);
+  //console.log(mydata[i].text);
+  console.log(mydata[i].text[0]);
+ }
+});
