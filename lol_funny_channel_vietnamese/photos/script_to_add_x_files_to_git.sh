d1dc4108badeedfddfc792f0f45899ab3f0ba729
thanhson <steven.n.t@outlook.com>
2022-11-13 22:50:34 +0700

    Final Batch

---
+++ lol_funny_channel_vietnamese/photos/script_to_add_x_files_to_git.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+files_per_batch=300
+
+set -- s*
+
+n=0
+N=0
+
+for filename do
+    git add $filename
+    n=$(( n + 1 ))
+    N=$(($n%$files_per_batch))
+    if test $N -eq 0
+    then
+      echo "Committing"
+      git commit -a -m "Done one batch"
+    fi
+done
+
+git commit -a -m "Final Batch"
+
